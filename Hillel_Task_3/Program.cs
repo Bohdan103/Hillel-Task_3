4d2391500849fa43980e4d3eae93bd3d83433736
Bohdan_Havryliuk <bogdangavrilyuk96@gmail.com>
2023-09-14 13:35:52 +0300

    add project files

---
+++ Hillel_Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hillel_Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
